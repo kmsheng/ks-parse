8017a193d2fd3005e2874c8901d77265844dd2d1
kmsheng <kmsh3ng@gmail.com>
2016-02-12 05:16:40 +0800

    init

---
+++ src/parse.js
@@ -0,0 +1,228 @@
+var _ = require('lodash')._;
+
+const LEFT_BRACE = 123;    // {
+const RIGHT_BRACE = 125;   // }
+const LEFT_BRACKET = 91;    // [
+const RIGHT_BRACKET = 93;    // ]
+const DOUBLE_QUOTE = 34;    // "
+const BACKSLASH = 92;    // \
+const COMMA = 44;    // ,
+const COLON = 58;    // :
+
+function Parse() {
+
+  this.stacks = [];
+  this.propStacks = [];
+  this.str = undefined;
+  this.nonStr = '';    // non str
+
+  this.objectOpened = false;
+  this.arrayOpened = false;
+  this.stringOpened = false;
+  this.skipOne = false;
+}
+
+Parse.prototype.parse = function(arr, options) {
+
+  arr = arr || [];
+  options = options || {chunkSize: 40};
+  var self = this;
+
+  _.each(arr, function(num) {
+
+    if (self.skipOne) {
+      self.skipOne = false;
+      return self.handleDefault(num);
+    }
+
+    switch (num) {
+      case BACKSLASH:
+        return self.handleBackSlash();
+      case COLON:
+        return self.handleColon();
+      case COMMA:
+        return self.handleComma(num);
+      case LEFT_BRACE:
+        return self.handleLeftBrace();
+      case RIGHT_BRACE:
+        return self.handleRightBrace();
+      case LEFT_BRACKET:
+        return self.handleLeftBracket();
+      case RIGHT_BRACKET:
+        return self.handleRightBracket();
+      case DOUBLE_QUOTE:
+        return self.handleDoubleQuote();
+      default:
+        return self.handleDefault(num);
+    }
+  });
+
+  return self.getResult(arr[arr.length - 1]);
+};
+
+Parse.prototype.getResult = function(lastNum) {
+  if (DOUBLE_QUOTE === lastNum) {
+    return this.str;
+  }
+  if (RIGHT_BRACKET === lastNum) {
+    return _.first(this.stacks);
+  }
+  if (RIGHT_BRACE === lastNum) {
+    return _.first(this.stacks);
+  }
+};
+
+Parse.prototype.parseNonStr = function(nonStr) {
+
+  if ('null' === nonStr) {
+    return null;
+  }
+
+  if ('true' === nonStr) {
+    return true;
+  }
+
+  if ('false' === nonStr) {
+    return false;
+  }
+
+  var value = parseFloat(nonStr);
+
+  if (_.isNumber(value) && ! _.isNaN(value)) {
+    return value;
+  }
+  console.trace('here');
+  throw 'Parsed failed: ' + nonStr;
+};
+
+Parse.prototype.handleColon = function() {
+  this.propStacks.push(this.str);
+  this.str = undefined;
+};
+
+Parse.prototype.handleComma = function(num) {
+
+  if (this.stringOpened) {
+    return this.handleDefault(num);
+  }
+
+  var stack = this.getCurrentStack();
+  var currentProp = this.getCurrentProp();
+
+  if (_.isPlainObject(stack) && currentProp && _.isString(this.str)) {
+    // write property and value
+    stack[currentProp] = this.str;
+    this.propStacks.pop();
+    this.str = undefined;
+  }
+  else if (_.isPlainObject(stack) && this.nonStr) {
+    // write property and value
+    stack[currentProp] = this.parseNonStr(this.nonStr);
+    this.propStacks.pop();
+    this.nonStr = '';
+  }
+  else if (_.isArray(stack) && _.isString(this.str)) {
+    stack.push(this.str);
+    this.str = undefined;
+  }
+  else if (_.isArray(stack) && this.nonStr) {
+    stack.push(this.parseNonStr(this.nonStr));
+    this.nonStr = '';
+  }
+};
+
+Parse.prototype.handleBackSlash = function() {
+  this.skipOne = true;
+};
+
+Parse.prototype.getParentStack = function() {
+  return this.stacks[this.stacks.length - 2];
+};
+
+Parse.prototype.getCurrentStack = function() {
+  return this.stacks[this.stacks.length - 1];
+};
+
+Parse.prototype.getCurrentProp = function() {
+  return this.propStacks[this.propStacks.length - 1];
+};
+
+Parse.prototype.handleLeftBrace = function() {
+  this.stacks.push({});
+  this.objectOpened = true;
+};
+
+Parse.prototype.handleLeftBracket = function() {
+  this.stacks.push([]);
+  this.arrayOpened = true;
+};
+
+Parse.prototype.handleRightBracket = function() {
+
+  if (this.nonStr) {
+    var stack = this.getCurrentStack();
+    stack.push(this.parseNonStr(this.nonStr));
+    this.nonStr = '';
+  }
+
+  this.arrayOpened = false;
+};
+
+Parse.prototype.write = function(stack, assignedValue) {
+
+  var value;
+
+  if (this.str) {
+    value = this.str;
+    this.str = undefined;
+  }
+  if (this.nonStr) {
+    value = this.parseNonStr(this.nonStr);
+    this.nonStr = '';
+  }
+  if (assignedValue) {
+    value = assignedValue;
+  }
+
+  if (_.isPlainObject(stack) && (undefined !== value)) {
+    var currentProp = this.getCurrentProp();
+    stack[currentProp] = value;
+    this.propStacks.pop();
+  }
+  if (_.isArray(stack) && (undefined !== value)) {
+    stack.push(value);
+    this.propStacks.pop();
+  }
+}
+
+Parse.prototype.handleRightBrace = function() {
+
+  this.objectOpened = false;
+  var parentStack = this.getParentStack();
+  var currentStack = this.getCurrentStack();
+
+  this.write(currentStack);
+
+  if (parentStack) {
+    this.write(parentStack, this.stacks.pop());
+  }
+};
+
+Parse.prototype.handleDoubleQuote = function() {
+  this.stringOpened = ! this.stringOpened;
+
+  if (this.stringOpened) {
+    this.str = '';
+  }
+};
+
+Parse.prototype.handleDefault = function(num) {
+  if (this.stringOpened) {
+    this.str += String.fromCharCode(num);
+  }
+  else {
+    this.nonStr += String.fromCharCode(num);
+  }
+};
+
+module.exports = Parse;
---
+++ test/parse.js
@@ -0,0 +1,73 @@
+var should = require('should');
+var Parse = require('../src/parse');
+
+function getUint8ArrayData(data) {
+  return new Uint8Array(new Buffer(JSON.stringify(data), 'utf8'));
+}
+
+describe('parse', function() {
+
+  it('should handle string', function() {
+
+    var parse = new Parse();
+    var data = 'testing';
+    var arr = getUint8ArrayData(data);
+
+    parse.parse(arr).should.equal('testing');
+  });
+
+  it('should handle array', function() {
+
+    var parse = new Parse();
+    var data = [
+      1,
+      2.1,
+      'str',
+      null,
+      {
+        nested: true,
+        nestedObject: {
+          omg: {
+            again: {},
+            test: 1
+          }
+        }
+      }
+    ];
+    var arr = getUint8ArrayData(data);
+
+    parse.parse(arr).should.deepEqual(data);
+  });
+
+  it('should handle object', function() {
+
+    var parse = new Parse();
+    var data = {
+      str: '',
+      num1: 1,
+      num2: 1.1,
+      nullProp: null,
+      undef: undefined,
+      func: function() {},
+      obj: {
+        num1: 2,
+        num2: 2.2
+      }
+    };
+
+    var arr = getUint8ArrayData(data);
+    var res = parse.parse(arr);
+
+    res.should.deepEqual({
+      str: '',
+      num1: 1,
+      num2: 1.1,
+      nullProp: null,
+      obj: {
+        num1: 2,
+        num2: 2.2
+      }
+    });
+  });
+
+});
